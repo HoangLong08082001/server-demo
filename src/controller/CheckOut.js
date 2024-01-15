6118d832c285880a701e9b9afb71e1ba4d8c2acc
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-01-15 13:46:21 +0700

    afternoon

--- src/controller/CheckOut.js
+++ src/controller/CheckOut.js
@@ -2,100 +2,93 @@ var dateFormat = require("date-format");
 import sortObject from "sortobject";
 var querystring = require("qs");
 const { json } = require("body-parser");
-// const ThanhToanVNPay = (req, res) => {
-//   // let ipAddr =
-//   //   req.headers["x-forwarded-for"] ||
-//   //   req.connection.remoteAddress ||
-//   //   req.socket.remoteAddress ||
-//   //   req.connection.socket.remoteAddress;
-
-//   // let tmnCode = "84Q3N3XL";
-//   // let secretKey = "QUESXHXBMPYCWHXMSOVJEEQBKDJKSLYJ";
-//   // let vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-//   // let date = new Date();
-//   // let createDate = format("yyyyMMddHHmmss", date);
-//   // let orderId = format("HHmmss", date);
-//   // let amount = req.body.sumofAmount;
-//   // let bankCode = "NCB";
-//   // let orderInfo = "CHUYEN KHUYEN DAT TOUR";
-//   // let orderType = "other";
-//   // let locale = "vn";
-//   // let currCode = "VND";
-//   // let vnp_Params = {};
-//   // vnp_Params["vnp_Version"] = "2.1.0";
-//   // vnp_Params["vnp_Command"] = "pay";
-//   // vnp_Params["vnp_TmnCode"] = tmnCode;
-//   // vnp_Params["vnp_Locale"] = locale;
-//   // vnp_Params["vnp_CurrCode"] = currCode;
-//   // vnp_Params["vnp_TxnRef"] = orderId;
-//   // vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
-//   // vnp_Params["vnp_OrderType"] = orderType;
-//   // vnp_Params["vnp_Amount"] = amount * 100;
-//   // vnp_Params["vnp_ReturnUrl"] = "http://localhost:3000/cart";
-//   // vnp_Params["vnp_IpAddr"] = ipAddr === "::1" ? "127.0.0.1" : "::1";
-//   // vnp_Params["vnp_CreateDate"] = createDate;
-//   // vnp_Params["vnp_BankCode"] = bankCode;
-//   // vnp_Params = sortObject(vnp_Params);
-//   // let signData = queryString.stringify(vnp_Params, { encode: false });
-//   // let hmac = crypto.createHmac("sha512", secretKey);
-//   // let signed = hmac.update(new Buffer.from(signData, "utf-8")).digest("hex");
-//   // vnp_Params["vnp_SecureHash"] = signed;
-//   // vnpUrl += "?" + queryString.stringify(vnp_Params, { encode: false });
-//   var ipAddr =
-//     req.headers["x-forwarded-for"] ||
-//     req.connection.remoteAddress ||
-//     req.socket.remoteAddress ||
-//     req.connection.socket.remoteAddress;
-
-//   var tmnCode = "84Q3N3XL";
-//   var secretKey = "QUESXHXBMPYCWHXMSOVJEEQBKDJKSLYJ";
-//   var vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-//   var returnUrl = "http://localhost:3000/cart";
-
-//   var date = new Date();
-
-//   var createDate = dateFormat(date, "yyyymmddHHmmss");
-//   var orderId = dateFormat(date, "HHmmss");
-//   var amount = 1000000;
-//   var bankCode = "NCB";
-
-//   var orderInfo = "THANH TOAN";
-//   var orderType = "1256";
-//   var locale = req.body.language;
-//   if (locale === null || locale === "") {
-//     locale = "vn";
-//   }
-//   var currCode = "VND";
-//   var vnp_Params = {};
-//   vnp_Params["vnp_Version"] = "2.1.0";
-//   vnp_Params["vnp_Command"] = "pay";
-//   vnp_Params["vnp_TmnCode"] = tmnCode;
-//   // vnp_Params['vnp_Merchant'] = ''
-//   vnp_Params["vnp_Locale"] = locale;
-//   vnp_Params["vnp_CurrCode"] = currCode;
-//   vnp_Params["vnp_TxnRef"] = orderId;
-//   vnp_Params["vnp_OrderInfo"] = orderInfo;
-//   vnp_Params["vnp_OrderType"] = orderType;
-//   vnp_Params["vnp_Amount"] = amount * 100;
-//   vnp_Params["vnp_ReturnUrl"] = returnUrl;
-//   vnp_Params["vnp_IpAddr"] = ipAddr === "::1" ? "127.0.0.1" : "::1";
-//   vnp_Params["vnp_CreateDate"] = createDate;
-//   if (bankCode !== null && bankCode !== "") {
-//     vnp_Params["vnp_BankCode"] = bankCode;
-//   }
-
-//   vnp_Params = sortObject(vnp_Params);
-
-//   var signData = querystring.stringify(vnp_Params, { encode: false });
-//   var crypto = require("crypto");
-//   var hmac = crypto.createHmac("sha512", secretKey);
-//   var signed = hmac.update(new Buffer.from(signData, "utf-8")).digest("hex");
-//   vnp_Params["vnp_SecureHash"] = signed;
-//   vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
-//   console.log(vnp_Params);
-//   console.log(vnpUrl);
-//   return res.status(200).json({ message: "success", data: vnpUrl });
-// };
+const ThanhToanVNPay = (req, res) => {
+  //   // let ipAddr =
+  //   //   req.headers["x-forwarded-for"] ||
+  //   //   req.connection.remoteAddress ||
+  //   //   req.socket.remoteAddress ||
+  //   //   req.connection.socket.remoteAddress;
+  //   // let tmnCode = "84Q3N3XL";
+  //   // let secretKey = "QUESXHXBMPYCWHXMSOVJEEQBKDJKSLYJ";
+  //   // let vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+  //   // let date = new Date();
+  //   // let createDate = format("yyyyMMddHHmmss", date);
+  //   // let orderId = format("HHmmss", date);
+  //   // let amount = req.body.sumofAmount;
+  //   // let bankCode = "NCB";
+  //   // let orderInfo = "CHUYEN KHUYEN DAT TOUR";
+  //   // let orderType = "other";
+  //   // let locale = "vn";
+  //   // let currCode = "VND";
+  //   // let vnp_Params = {};
+  //   // vnp_Params["vnp_Version"] = "2.1.0";
+  //   // vnp_Params["vnp_Command"] = "pay";
+  //   // vnp_Params["vnp_TmnCode"] = tmnCode;
+  //   // vnp_Params["vnp_Locale"] = locale;
+  //   // vnp_Params["vnp_CurrCode"] = currCode;
+  //   // vnp_Params["vnp_TxnRef"] = orderId;
+  //   // vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
+  //   // vnp_Params["vnp_OrderType"] = orderType;
+  //   // vnp_Params["vnp_Amount"] = amount * 100;
+  //   // vnp_Params["vnp_ReturnUrl"] = "http://localhost:3000/cart";
+  //   // vnp_Params["vnp_IpAddr"] = ipAddr === "::1" ? "127.0.0.1" : "::1";
+  //   // vnp_Params["vnp_CreateDate"] = createDate;
+  //   // vnp_Params["vnp_BankCode"] = bankCode;
+  //   // vnp_Params = sortObject(vnp_Params);
+  //   // let signData = queryString.stringify(vnp_Params, { encode: false });
+  //   // let hmac = crypto.createHmac("sha512", secretKey);
+  //   // let signed = hmac.update(new Buffer.from(signData, "utf-8")).digest("hex");
+  //   // vnp_Params["vnp_SecureHash"] = signed;
+  //   // vnpUrl += "?" + queryString.stringify(vnp_Params, { encode: false });
+  //   var ipAddr =
+  //     req.headers["x-forwarded-for"] ||
+  //     req.connection.remoteAddress ||
+  //     req.socket.remoteAddress ||
+  //     req.connection.socket.remoteAddress;
+  //   var tmnCode = "84Q3N3XL";
+  //   var secretKey = "QUESXHXBMPYCWHXMSOVJEEQBKDJKSLYJ";
+  //   var vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+  //   var returnUrl = "http://localhost:3000/cart";
+  //   var date = new Date();
+  //   var createDate = dateFormat(date, "yyyymmddHHmmss");
+  //   var orderId = dateFormat(date, "HHmmss");
+  //   var amount = 1000000;
+  //   var bankCode = "NCB";
+  //   var orderInfo = "THANH TOAN";
+  //   var orderType = "1256";
+  //   var locale = req.body.language;
+  //   if (locale === null || locale === "") {
+  //     locale = "vn";
+  //   }
+  //   var currCode = "VND";
+  //   var vnp_Params = {};
+  //   vnp_Params["vnp_Version"] = "2.1.0";
+  //   vnp_Params["vnp_Command"] = "pay";
+  //   vnp_Params["vnp_TmnCode"] = tmnCode;
+  //   // vnp_Params['vnp_Merchant'] = ''
+  //   vnp_Params["vnp_Locale"] = locale;
+  //   vnp_Params["vnp_CurrCode"] = currCode;
+  //   vnp_Params["vnp_TxnRef"] = orderId;
+  //   vnp_Params["vnp_OrderInfo"] = orderInfo;
+  //   vnp_Params["vnp_OrderType"] = orderType;
+  //   vnp_Params["vnp_Amount"] = amount * 100;
+  //   vnp_Params["vnp_ReturnUrl"] = returnUrl;
+  //   vnp_Params["vnp_IpAddr"] = ipAddr === "::1" ? "127.0.0.1" : "::1";
+  //   vnp_Params["vnp_CreateDate"] = createDate;
+  //   if (bankCode !== null && bankCode !== "") {
+  //     vnp_Params["vnp_BankCode"] = bankCode;
+  //   }
+  //   vnp_Params = sortObject(vnp_Params);
+  //   var signData = querystring.stringify(vnp_Params, { encode: false });
+  //   var crypto = require("crypto");
+  //   var hmac = crypto.createHmac("sha512", secretKey);
+  //   var signed = hmac.update(new Buffer.from(signData, "utf-8")).digest("hex");
+  //   vnp_Params["vnp_SecureHash"] = signed;
+  //   vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+  //   console.log(vnp_Params);
+  //   console.log(vnpUrl);
+  //   return res.status(200).json({ message: "success", data: vnpUrl });
+};
 const ThanhToanMoMo = (req, res) => {
   var accessKey = "F8BBA842ECF85";
   var secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
