d14a849f2e271f2e8bd40efc79fa4bd0b0e3a031
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-01-15 16:40:13 +0700

    update now

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,17 +1,19 @@
-import mysql from "mysql";
+const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
-const pool = mysql.createPool({
-  database: process.env.DATABASE || "demo_giamgia",
-  host: process.env.HOST || "localhost",
-  port: process.env.PORT || 3306,
-  user: process.env.USER || "root",
-  password: process.env.PASS || "",
-  connectionLimit: process.env.LIMIT || 10,
+const pool = mysql.createConnection({
+  host: "bcndwcxfcewfwt2n5nhq-mysql.services.clever-cloud.com",
+  user: "uaiv8dfxqgm5y67j",
+  password: "cBNwOxEqNnLD5jp26Bep",
+  database: "bcndwcxfcewfwt2n5nhq",
 });
-pool.getConnection((err) => {
+pool.connect((err) => {
   if (err) {
     console.log("Connect Fails");
+    console.log(process.env.DATABASE);
+    console.log(process.env.PORT_VIEWS);
+    console.log(process.env.PORT_VIEWS);
+    console.log(process.env.PORT_VIEWS);
   } else {
     console.log("Connect Success");
 
